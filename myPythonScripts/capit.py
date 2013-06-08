6f459b7a4d4147ada47e852db4fd1ce22f2e32f1
ChristinaK <christinkconnect@gmail.com>
2013-06-08 00:24:17 -0700

    adding remaining files

---
+++ myPythonScripts/capit.py
@@ -0,0 +1,4 @@
+import allcaps 
+
+for word in '''several species of small furry animals gathered together in a cave and grooving with a pict'''.split():
+    print allcaps.capitalize(word)
---
+++ myPythonScripts/shell.py
@@ -0,0 +1,26 @@
+import subprocess
+
+def ls():
+    output = subprocess.check_output(['ls']) # output is a string of everything returned
+    directory = output.split('\n') # split the string on newlines
+    directory.pop() # throw away the last, empty line
+    for filename in directory:
+        print filename
+
+def lsl():
+    output = subprocess.check_output(['ls', '-l', '-h'])
+    directory = output.split('\n') # split the string on newlines
+    directory.pop() # throw away the last, empty line
+    for filename in directory:
+        print filename
+
+def main():
+    print 'ls:'
+    ls()
+    print
+    print 'ls -l -h:'
+    lsl()
+    print
+
+if __name__ == '__main__':
+    main()
